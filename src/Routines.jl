4ce8621ddb650c32da2fd52b94087904ffac377e
Martin Bies <MBies87@googlemail.com>
2022-05-30 14:56:46 +0200

    Code improvements: Can now handle edge lists such as [[1,4],[3,13]]
    Vertices are then [1,3,4,13] and a vertex with name "2" or "7" does not exist

--- src/Routines.jl
+++ src/Routines.jl
@@ -1,49 +1,50 @@
 # Assumes that a connected graph is given as input
 function Counter(edges::Vector{Vector{Int64}})
-    # find the indicies of the vertices
+    # Make dictionary with index for each vertex
     vertices = unique(reduce(vcat,edges))
-    
-    # find the number of edges attached to each vertex
-    nodes_per_vertex = fill(0,length(vertices))
-    for e in edges
-        nodes_per_vertex[findfirst(x->x==e[1],vertices)] += 1
-        nodes_per_vertex[findfirst(x->x==e[2],vertices)] += 1
+    vert_dict = Dict{Int64, Int64}()
+    for i in 1:length(vertices)
+        vert_dict[vertices[i]] = i
     end
 
-    # compute degree of KC
+    # Compute degree of KC
+    nodes_per_vertex = fill(0,length(vertices))
+    for e in edges
+        nodes_per_vertex[vert_dict[e[1]]] += 1
+        nodes_per_vertex[vert_dict[e[2]]] += 1
+    end
     deg_KC = [-2+e for e in nodes_per_vertex]
 
-    # create a matrix to collect results
+    # Initialize variables
     res_matrix = zero_matrix(ZZ, length(edges)+1, sum([deg_KC[k]+1 for k in findall(>=(0), deg_KC)])+1)
-
-    # compute multiplicity
     mult = 2^(length(edges) + 1 - length(vertices))
-
-    # Initialize total
     total = 0
+
+    # Iterate over all roots (below, c are the edges/nodes that we blow up)
     for k in 0:length(edges)
         combinations = Oscar.Hecke.subsets([e for e in 1:length(edges)],k)
         for c in combinations
-            # compute the new degrees from these blowups
+
+            # Compute the new degrees from the blowups
             new_degs = [d for d in deg_KC]
             for edge_index in c
-                new_degs[edges[edge_index][1]] = new_degs[edges[edge_index][1]] - 1
-                new_degs[edges[edge_index][2]] = new_degs[edges[edge_index][2]] - 1
+                new_degs[vert_dict[edges[edge_index][1]]] = new_degs[vert_dict[edges[edge_index][1]]] - 1
+                new_degs[vert_dict[edges[edge_index][2]]] = new_degs[vert_dict[edges[edge_index][2]]] - 1
             end
 
-            # check if all degrees are divisible by two
+            # Check if all degrees are divisible by two
             if all(y->iseven(y),new_degs)
 
-                # compute h0
-                new_edges = []
+                # Compute h0
+                new_edges = Vector{Int64}[]
                 for i in 1:length(edges)
                     if !(i in c)
                         push!(new_edges, edges[i])
                     end
                 end
-                h0 = H0(Vector{Vector{Int64}}(new_edges), Vector{Int64}([div(d,2) for d in new_degs]))
+                h0 = H0(vert_dict, new_edges, [div(d,2) for d in new_degs])
 
-                # update result matrix and total
+                # Update result matrix and total
                 res_matrix[length(new_edges)+1,h0+1] += mult
                 total += mult
             end
@@ -62,15 +63,16 @@ export Counter
 
 
 # Compute generic h0
-function H0(edges::Vector{Vector{Int64}}, degrees::Vector{Int64})
+function H0(vert_dict::Dict{Int64, Int64}, edges::Vector{Vector{Int64}}, degrees::Vector{Int64})
+
     # Find indices of the components with non-negative degree
     indices = findall(>=(0), degrees)
 
     # Construct a zero matrix
-    number_of_local_sections = sum([degrees[i] + 1 for i in indices])
+    number_of_local_sections = sum([degrees[i]+1 for i in indices])
     matrix = zero_matrix(ZZ, length(edges), number_of_local_sections)
 
-    # Identify index in matrix for non-trivial sections on each component
+    # Identify column index for non-trivial sections on each component
     indices_dict = Dict{Int64, Int64}()
     if length(indices) > 0
         indices_dict[indices[1]] = 1
@@ -84,16 +86,16 @@ function H0(edges::Vector{Vector{Int64}}, degrees::Vector{Int64})
     # For each node add entries to the matrix
     for i in 1:length(edges)
         e = edges[i]
-        if degrees[e[1]] >= 0
+        if degrees[vert_dict[e[1]]] >= 0
             pos_v = rand(-20:20)
-            for j in 0:degrees[e[1]]
-                matrix[i, indices_dict[e[1]]+j] += pos_v^j
+            for j in 0:degrees[vert_dict[e[1]]]
+                matrix[i, indices_dict[vert_dict[e[1]]]+j] += pos_v^j
             end
         end
-        if degrees[e[2]] >= 0
+        if degrees[vert_dict[e[2]]] >= 0
             pos_v = rand(-20:20)
-            for j in 0:degrees[e[2]]
-                matrix[i, indices_dict[e[2]]+j] += (-1) * pos_v^j
+            for j in 0:degrees[vert_dict[e[2]]]
+                matrix[i, indices_dict[vert_dict[e[2]]]+j] += (-1) * pos_v^j
             end
         end
     end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,6 @@ using Test
 
 @testset "Compute some Brill-Noether numbers" begin
     @test Counter([[1,1]]) == matrix(ZZ, [[2,0],[0,2]])
+    @test Counter([[1,3]]) == matrix(ZZ, [[1],[0]])
     @test Counter([[1,2],[1,2]]) == matrix(ZZ,[[2,0,0],[0,0,0],[0,2,0]])
 end
